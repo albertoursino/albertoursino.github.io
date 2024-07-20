343bf4938f59301b0da74dacb61f7b73be81b132
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2024-07-20 11:20:47 +0200

    feat: cv not a download button anymore
    - took inspiration from here: https://brittanychiang.com/

--- webapp/ursinoalberto/views.py
+++ webapp/ursinoalberto/views.py
@@ -1,7 +1,17 @@
-from django.http import HttpRequest, HttpResponse
+from pathlib import Path
+
+from django.http import FileResponse, HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> HttpResponse:
     """Home view."""
     return render(request, "ursinoalberto/home.html", context={})
+
+
+def resume(request: HttpRequest) -> HttpResponse:
+    """Resume view."""
+    return FileResponse(
+        Path.open("webapp/ursinoalberto/static/ursinoalberto/resume.pdf", "rb"),
+        content_type="application/pdf",
+    )
