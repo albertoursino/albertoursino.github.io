2b4db985ec452e73845fc037b87ec4999e61e838
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2024-07-28 19:14:24 +0200

    feat: uploaded bachelor's thesis

--- webapp/ursinoalberto/urls.py
+++ webapp/ursinoalberto/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("resume.pdf", views.resume, name="resume"),
     path("master_thesis.pdf", views.master_thesis, name="master_thesis"),
+    path("bachelor_thesis.pdf", views.bachelor_thesis, name="bachelor_thesis"),
 ]
--- webapp/ursinoalberto/views.py
+++ webapp/ursinoalberto/views.py
@@ -1,6 +1,4 @@
-from pathlib import Path
-
-from django.http import FileResponse, HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
@@ -13,6 +11,12 @@ def resume(request: HttpRequest) -> HttpResponse:
     """Resume view."""
     return render(request, "ursinoalberto/resume.html", context={})
 
+
 def master_thesis(request: HttpRequest) -> HttpResponse:
-    """Resume view."""
+    """Master's thesis view."""
     return render(request, "ursinoalberto/master_thesis.html", context={})
+
+
+def bachelor_thesis(request: HttpRequest) -> HttpResponse:
+    """Bachelor's thesis view."""
+    return render(request, "ursinoalberto/bachelor_thesis.html", context={})
