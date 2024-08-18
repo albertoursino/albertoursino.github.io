02aec69769af5141aa8e61ab08fef92114775173
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2024-08-18 19:30:44 +0200

    feat: removed template inheritance
    it was not necesarry

--- webapp/ursinoalberto/urls.py
+++ webapp/ursinoalberto/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("about", views.about, name="about"),
     path("master_thesis.pdf", views.master_thesis, name="master_thesis"),
     path("bachelor_thesis.pdf", views.bachelor_thesis, name="bachelor_thesis"),
 ]
--- webapp/ursinoalberto/views.py
+++ webapp/ursinoalberto/views.py
@@ -3,13 +3,8 @@ from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    """Home view."""
+    """Render the main view of the website."""
     return render(request, "ursinoalberto/home.html", context={})
-
-
-def about(request: HttpRequest) -> HttpResponse:
-    """About view."""
-    return render(request, "ursinoalberto/about.html", context={})
 
 
 def master_thesis(request: HttpRequest) -> HttpResponse:
