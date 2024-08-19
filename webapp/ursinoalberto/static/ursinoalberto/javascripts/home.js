a77f15dc702066b2f97189158bb556a5f52c9cdc
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2024-08-19 16:49:03 +0200

    feat: removed arrow
    - implemented first version of the about section

--- webapp/ursinoalberto/static/ursinoalberto/javascripts/home.js
+++ webapp/ursinoalberto/static/ursinoalberto/javascripts/home.js
@@ -1,10 +1,14 @@
-/* Android: workaround to avoid browsers toolbar messing up with the UI */
-window.scrollTo(0, 1);
+// global variable for animations speed
+let speed = 500;
 
-/* Mouse scroll listeners */
+/** Function for the listener of type "wheel"
+ *
+ * @param event: WheelEvent (https://developer.mozilla.org/en-US/docs/Web/API/WheelEvent)
+ */
 function mouseScroll(event) {
   scrollPage(event.deltaY);
 }
+
 document.addEventListener("wheel", mouseScroll);
 
 /**
@@ -25,63 +29,82 @@ function switchMode() {
   }
 }
 
-/* Animates the arrow, rotating it upside down or upwards */
-function rotateArrow(side) {
-  if (side == "up") {
-    document.getElementsByClassName("fas fa-caret-down")[0].animate(
-      [
-        {
-          transform: "rotate(0)",
-        },
-        {
-          transform: "rotate(0.5turn)",
-        },
-      ],
-      { duration: 500, fill: "forwards" }
-    );
-  } else if (side == "down") {
-    document.getElementsByClassName("fas fa-caret-down")[0].animate(
-      [
-        {
-          transform: "rotate(0.5turn)",
-        },
-        {
-          transform: "rotate(0)",
-        },
-      ],
-      { duration: 500, fill: "forwards" }
-    );
+/**
+ * Sets an animation to the landing page which decreases its opacity to zero.
+ *
+ * @param type: 1 if you are scrolling to the about section, 2 if scrolling to the landing page
+ */
+function fadeLandingPage(type) {
+  if (type == 1) {
+    document
+      .getElementsByClassName("landing-page")
+      .item(0)
+      .animate(
+        [
+          {},
+          {
+            opacity: "0",
+          },
+        ],
+        { duration: speed + 300 }
+      );
+  } else if (type == 2) {
+    document
+      .getElementsByClassName("landing-page")
+      .item(0)
+      .animate(
+        [
+          { opacity: "0" },
+          {
+            opacity: "100",
+          },
+        ],
+        { duration: speed + 200 }
+      );
   }
 }
 
 /**
  * Smoothly scrolls the page between the about section and the landing page.
+ *
+ * @param mouseScrollY: double representing the vertical scroll amount in the WheelEvent.deltaMode unit
  */
 function scrollPage(mouseScrollY = null) {
   // Avoids to scroll while the page is scrolling because I want to avoid two problems:
   // - "window.scrollY <= 1"
-  // - the arrow rotation
   document
-    .getElementsByClassName("arrow-down normal-btn")[0]
+    .getElementsByClassName("about-btn normal-btn")[0]
     .setAttribute("onclick", null);
   document.removeEventListener("wheel", mouseScroll);
 
-  if (
-    (window.scrollY <= 1 && mouseScrollY == null) ||
-    Math.sign(mouseScrollY) == 1
-  ) {
-    window.scrollTo(0, window.innerHeight);
-    rotateArrow("up");
+  const about_section = document.getElementById("about_section");
+
+  if (mouseScrollY == null) {
+    if (window.scrollY <= 1) {
+      about_section.scrollIntoView();
+      fadeLandingPage(1);
+    } else {
+      window.scrollTo(0, 1);
+      fadeLandingPage(2);
+    }
   } else {
-    window.scrollTo(0, 1);
-    rotateArrow("down");
+    if (Math.sign(mouseScrollY) == 1 && window.scrollY < window.innerHeight) {
+      about_section.scrollIntoView();
+      fadeLandingPage(1);
+    } else if (
+      Math.sign(mouseScrollY) == -1 &&
+      window.scrollY >= window.innerHeight
+    ) {
+      window.scrollTo(0, 1);
+      fadeLandingPage(2);
+    }
   }
 
   // Reactivates the scroll
   setTimeout(() => {
     document
-      .getElementsByClassName("arrow-down normal-btn")[0]
+      .getElementsByClassName("about-btn normal-btn")[0]
       .setAttribute("onclick", "scrollPage()");
     document.addEventListener("wheel", mouseScroll);
-  }, "650");
+  }, speed.toString());
 }
