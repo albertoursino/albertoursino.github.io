09ca1805f9f397cb3608b9738d0c8dda592437b1
Alberto Ursino <Alberto.Ursino@k-ai.at>
2025-09-01 15:39:20 +0200

    feat: remove Django and make the website super simple

--- webapp/ursinoalberto/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UrsinoalbertoConfig(AppConfig):  # noqa: D101
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "ursinoalberto"
--- webapp/ursinoalberto/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("master_thesis.pdf", views.master_thesis, name="master_thesis"),
-    path("bachelor_thesis.pdf", views.bachelor_thesis, name="bachelor_thesis"),
-]
